d5f23b0765828ff3375189cc9a3d046a0b055817
song_xiao_lin <song_xiao_lin@126.com>
2023-11-01 15:54:39 +0800

    style：样式调整

--- components/Wanjinganquan/Header/index.vue
+++ components/Wanjinganquan/Header/index.vue
@@ -3,13 +3,13 @@
 
 <template>
   <div class="wjaq-container mx-auto h-[86px] flex justify-between items-center">
-    <div class="header-left">
+    <div class="header-left mt-[10px]">
       <NuxtLink to="/">
-        <img src="@/assets/Images/logo.png" class="w-[130px]" />
+        <img src="@/assets/Images/logo.png" class="w-[125px]" />
       </NuxtLink>
     </div>
     <div class="header-right flex">
-      <div class="ml-[120px] text-[20px]" v-for="(item, index) in [
+      <div class="ml-[120px] text-[18px]" v-for="(item, index) in [
         { text: '安全产品', to: '/' },
         { text: '安全服务', to: '/securityService' },
         { text: '开源社区', to: '/openSourceCommunity' },
